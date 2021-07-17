2083bee2ceb4e8215fffbfb217ab8c16e112c56d
Raghul Jayan <raghuldpi95@gmail.com>
2021-07-17 13:40:42 +0530

    :memo: Added Jsdoc defintions

--- src/utils/fillSeatMatrix.js
+++ src/utils/fillSeatMatrix.js
@@ -2,7 +2,27 @@ import flattenDeep from 'lodash/flattenDeep';
 import sortBy from 'lodash/sortBy';
 import map from 'lodash/map';
 
+/**
+ * Seat is Class construct which is helpful in computing the seat matrix and this aligns strongly with OOPS concepts
+ * Class declaration
+ * @class
+ * @constructor
+ * @public
+ */
 class Seat {
+	/**
+		 * The above Seat class includes following properties
+		 * matrixId - @type {String} - keep track of the origin of the seat (refers the group to which this seat belong)
+		 * row - @type {int} - row position of the seat within the origin group
+		 * column - @type {int} - column position of the seat within the origin group
+		 * passengerIdx - @type {int} - stores the passenger identity to whom the seat belongs in passenger queue
+		 * category - @type {string} - type of the seat (aisle, center, window) and this will really helps in User Interface 
+		 * @public
+		 * @example
+		 * {"category": "aisle", "column": 1, "matrixId": 0, "passengerIdx": 1, "row": 1}
+		 * {"category": "window", "column": 1, "matrixId": 0, "passengerIdx": 1, "row": 0}
+		 * 
+    */
 	constructor(matrixId, row, column, category) {
 		this.matrixId = matrixId
 		this.row = row
@@ -12,13 +32,30 @@ class Seat {
 	}
 }
 
-const fillSeatMatrix = (groupList, passengerCount) => {
+/**
+ * The below function expression is exported and used during the user event of filling the seats
+ * This should be used with the below parameters as arguments during the user events
+ * The default values of passengerCount tends to be zero
+ * groupList is the mandatory parameter for the below function
+ * @param {Array} groupList 
+ * @param {int} passengerCount 
+ * @returns {Array<Seat>}
+ */
+const fillSeatMatrix = (groupList, passengerCount = 0) => {
 	let aisle = [];
 	let window = [];
 	let center = [];
 	let count = 1;
 
-	const getAisleSeats = (matrixGroup, groupIndex) => {
+	/**
+	 * The below functions creates the Aisle Seat nodes for all the seat groups
+	 * This is the un-flattened Array each may be created on different row or columns or even in different groups 
+	 * @param {Array<[m, n]>} matrixGroup 
+	 * m - row length and n - column length
+	 * @param {int} groupIndex 
+	 * @returns {Array<Seat>} specific to Aisle Category
+	 */
+	const createAisleSeats = (matrixGroup, groupIndex) => {
 		if (groupIndex === 0) {
 			let tempArray = [];
 			if (matrixGroup[1] === 1) return;
@@ -52,7 +89,15 @@ const fillSeatMatrix = (groupList, passengerCount) => {
 		}
 	}
 
-	const getWindowSeats = (matrixGroup, groupIndex) => {
+
+	/**
+	 * The below functions creates the Window Seat nodes for all the seat groups
+	 * This is the un-flattened Array each may be created on different row or columns or even in different groups 
+	 * @param {Array<[m, n]>} matrixGroup 
+	 * m - row length and n - column length
+	 * @param {int} groupIndex 
+	 */
+	const createWindowSeats = (matrixGroup, groupIndex) => {
 		if (groupIndex === 0) {
 			let tempArray = [];
 			for (let i = 0; i < matrixGroup[0]; i++) {
@@ -77,6 +122,13 @@ const fillSeatMatrix = (groupList, passengerCount) => {
 		}
 	}
 
+	/**
+	 * The below functions creates the Center Seat nodes for all the seat groups
+	 * This is the easier part of creating the rest of the seats for remaining index in seat group matrices
+	 * @param {Array<[m, n]>} matrixGroup 
+	 * m - row length and n - column length
+	 * @param {int} groupIndex 
+	 */
 	const getCenterSeats = (matrixGroup, groupIndex) => {
 		for (let i = 0; i < matrixGroup[0]; i++) {
 			let tempArray = [];
@@ -94,11 +146,16 @@ const fillSeatMatrix = (groupList, passengerCount) => {
 	const fillMatrix = () => {
 		for (let i = 0; i < groupList.length; i++) {
 
-			// The first condition says that one group will have two windows and no aisles 
+			/**
+			 * Iterating each seatGroup and filling based on the order below.
+			 * When its one columned one seatGroup it will be a window
+			 * When its the two columned one seatGroup both will be a window
+			 * Like the above few etch cases are also covered to avoid confusion
+			 */
 			if (groupList.length !== 1 && groupList[i][1] >= 1) {
-				getAisleSeats(groupList[i], i);
+				createAisleSeats(groupList[i], i);
 			}
-			getWindowSeats(groupList[i], i);
+			createWindowSeats(groupList[i], i);
 			if (groupList[i][1] > 2) {
 				getCenterSeats(groupList[i], i);
 			}
@@ -108,12 +165,19 @@ const fillSeatMatrix = (groupList, passengerCount) => {
     
 	fillMatrix()
 
+	/**
+	 * The sorting is kept aside from the above logic to ensure that sorting can be controlled easily
+	 * based on the preference like top to down or vice-versa
+	 */
 	const sortedList = [
 		...sortBy(flattenDeep(aisle), ['row', 'matrixId']),
 		...sortBy(flattenDeep(window), ['row', 'matrixId']),
 		...sortBy(flattenDeep(center), ['row', 'matrixId'])
 	]
 
+	/**
+	 * Involves in filling the seats within the sortedList and rest of the seats will stay unfilled
+	 */
 	map(sortedList, seat => {
 		if (count > passengerCount) return;
 		seat.passengerIdx = count
